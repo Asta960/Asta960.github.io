c24e3c24cbbef6fc9f865988f1cf4028add82fa1
Artem Piankov <asta960@mail.ru>
2024-10-20 01:11:00 +0300

    popup  1.2

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ $(document).ready(() => {
   }
 
   //Предупреждение о разработке
-  tg.PopupButton([{"type":"close", "text":"TXT_POPUP"}])
+  // tg.PopupButton([{"type":"close", "text":"TXT_POPUP"}])
+  tg.web_app_open_popup({"type":"close", "text":"TXT_POPUP"})
 
   // отключение свайпа для закрытия
   tg.isVerticalSwipesEnabled = false;
