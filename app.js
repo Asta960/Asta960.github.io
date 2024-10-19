48c82fc937ee9f9ea2f4fbc7e780329ab7d51740
Artem Piankov <asta960@mail.ru>
2024-10-20 00:21:06 +0300

    no swipe 1.0

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 $(document).ready(() => {
   let tg = window.Telegram.WebApp;
 
+  // отключение свайпа для закрытия
+  tg.isVerticalSwipesEnabled = false;
+
   $('.btn-close').click(() => {
     tg.close();
   })
