66e59ef0b23c0acf5e1cf6cdb90e7b486191a600
Artem Piankov <asta960@mail.ru>
2024-10-20 03:21:01 +0300

    start webpack 1.7

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,35 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/js/app.js', // Ваш входной файл
+    entry: './src/js/app.js', // Входной файл
     output: {
         filename: 'bundle.js', // Имя выходного файла
         path: path.resolve(__dirname, 'dist'), // Папка для выхода
     },
     mode: 'development', // Или 'production'
+    module: {
+        rules: [
+            {
+                test: /\.scss$/, // Применяем правило к файлам .scss
+                use: [
+                    MiniCssExtractPlugin.loader, // Извлекаем CSS в файл
+                    'css-loader',                // Обрабатывает CSS
+                    'sass-loader',               // Компилирует SCSS в CSS
+                ],
+            },
+            {
+                test: /\.js$/, // Применяем правило к файлам .js
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader', // Если используете Babel
+                },
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css', // Имя выходного CSS файла
+        }),
+    ],
 };
