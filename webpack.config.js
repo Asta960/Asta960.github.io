9f526943457d4e08247fd2c0c9550ea03ff76005
Artem Piankov <asta960@mail.ru>
2024-10-20 03:22:42 +0300

    start webpack 1.8

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: 'styles.css', // Имя выходного CSS файла
+            filename: 'style.css', // Имя выходного CSS файла
         }),
     ],
 };
